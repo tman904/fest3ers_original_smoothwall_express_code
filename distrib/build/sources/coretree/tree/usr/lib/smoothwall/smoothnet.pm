afa01b3da01f97d31112950fa9845d03addb8614
darren <darren@2e32aa7d-f303-0410-ac85-8a20d3371fc6>
2005-08-19 11:30:56 +0000

    updated to use wget for downloading
    git-svn-id: https://smoothwall.svn.sourceforge.net/svnroot/smoothwall/trunk@107 2e32aa7d-f303-0410-ac85-8a20d3371fc6

--- distrib/build/sources/coretree/tree/usr/lib/smoothwall/smoothnet.pm
+++ distrib/build/sources/coretree/tree/usr/lib/smoothwall/smoothnet.pm
@@ -2,238 +2,21 @@
 
 package smoothnet;
 require Exporter;
+use File::Copy;
 @ISA = qw(Exporter);
 
 # define the Exportlists.
 
 @EXPORT       = qw();
-@EXPORT_OK    = qw( download $downloadstore );
+@EXPORT_OK    = qw( download progress progress_bar update_bar $progress_store $download_store $server cancel clear_download_cache );
 %EXPORT_TAGS  = (
-		standard   => [qw( download $downloadstore)],
+		standard   => [ @EXPORT_OK ],
 		);
 
 our $server        = "downloads.smoothwall.org";
-our $basever       = "2.0";
-our $uri           = "/updates/$basever/";
-our $extension     = ".tar.gz";
 
-my $default_port   = "80";
-my $protocol       = "http";
-
-my $retries       = 3;	# number of retries for downloads
-my $proxyserver   = "";
-my $proxyport     = "";
-my $agent         = "SmoothWall Download Agent V1.1";
-my $basestore     = "/var/patches";
-my $downloadstore = "$basestore/downloads";
-my $pendingstore  = "$basestore/pending";
-
-# HTTP / HTTPS download mechanism to download the latest updates.
-use LWP::Simple;
-use LWP::UserAgent;
-use HTTP::Request;
-
-# modules
-use IO::Socket;
-
-sub download
-{
-	my ( $filename, $size, $md5, $status ) = @_;
-
-	my $url = "$protocol://$server/$uri/$basever-$filename$extension"; 
-
-	my $destination = "$downloadstore/$filename";
-
-	$url =~s/ /%20/g;
-	$filename =~s/ /%20/g;
-
-        for my $retryno (1..$retries) {
-		if ( &safedownload( "$url", $size, $destination, $proxyserver, $proxyport, $status, $filename ) == 1) {
-
-			my $gotsize = ( stat( "$destination") )[7];
-
-			if($gotsize != $size) {
-				system('/usr/bin/logger', '-t', 'SmoothDownload', "Download failed (read $gotsize bytes instead of $size) | $retryno" );
-				sleep(3*$retryno);
-                                next;
-                        }       
-			
-			# calculate the MD5 of the file we just downloaded
-                        my $md5sum;
-
-			open(PIPE, '-|') || exec('/usr/bin/md5sum', "$destination");
-			
-                        while (<PIPE>) { $md5sum = $md5sum.$_; }
-                        close (PIPE);
-                        
-			chomp $md5sum;
-                        $md5sum =~ /^([\da-fA-F]+)/;
-                        $md5sum = $1;
-                        if ($md5sum eq $md5) {
-                        	# file is ok, it has the correct md5
-				system('/usr/bin/logger', '-t', 'SmoothDownload', "Complete $destination" );
-				return $destination;
-                        } else {
-				# failed;
-				system('/usr/bin/logger', '-t', 'SmoothDownload', "Download failed MD5 mistmatch $md5 vs $md5sum" );
-				return undef;
-                        }
-		} else {
-			system('/usr/bin/logger', '-t', 'SmoothDownload', "Download failed, Retry $retryno" );
-		}
-	}
-
-	system('/usr/bin/logger', '-t', 'SmoothDownload', "Unable to download update ($destination from $url)" );
-
-	open ( my $out, ">$destination.failure" ) or return undef;
-	print $out "too many retries";
-
-	close $out;
-
-	unlink "$destination";
-	unlink "$destination.abort";
-	
-	system('/usr/bin/logger', '-t', 'SmoothDownload', "end $destination" );
-
-	return undef;
-}
-
-sub safedownload {
-        my ( $url, $expectedbytes, $saveasfile, $proxyserver, $proxyport, $status, $filename ) = @_;
-	
-        my $bytesdown = 1;
-        my $data;
-        my $req = new HTTP::Request( GET => $url );
-        my $ua = new LWP::UserAgent;
-        
-	$ua->agent("$agent" . $ua->agent);
-        $ua->timeout(300);
-	$ua->env_proxy;
-
-        $start_t = time;
-        $last_time = $start_t;
-
-	unlink( "$saveasfile" );
-	unlink "$saveasfile.failure";
-	unlink "$saveasfile.timestamp";	
-	
-        if ($proxyserver) {
-                $ua->proxy(['http', 'https'], 'http://$proxyserver:$proxyport/');
-        }
-        unless ( open(FILE, ">$saveasfile") ){
-		warn "Unable to open updates file ($saveasfile)\n";
-		return ( 0 );
-	};
-        if ( open(TSFILE, ">$saveasfile.timestamp") ){
-		print TSFILE $start_t;
-		close TSFILE;
-	} else {
-		warn "Unable to open updates file ($saveasfile)\n";
-	};
-
-        binmode FILE;
-
-	&{$status}( 0,0,0,0,$expectedbytes, $filename );
-
-        my $res = $ua->request($req, 
-                sub {
-	    		my ($d, $res) = @_;
-                        if (!($res->is_success)) {
-                                close(FILE);
-                                return 0;
-                        }
-			if ( -e "$saveasfile.abort" ){
-				close FILE;
-				unlink "$saveasfile";
-				unlink "$saveasfile.abort";
-				unlink "$saveasfile.failure";
-				unlink "$saveasfile.timestamp";
-				exit -1;
-			}
-                        print FILE $d;
-                        $bytesdown += length($d);
-                        if ( (time - $last_time) > 1) {
-                                $last_time = time;
-				size_callback( $bytesdown, $expectedbytes, $status, $start_t, $filename);	
-                        }
-                }
-        , 4096 );
-        close(FILE);
-	unlink "$saveasfile.timestamp";
-     
-	if ($res->is_success) {
-                return 1;
-        }
-
-        return 0;
-}
-
-sub size_callback
-{
-	my ( $bytesdown, $expectedbytes, $status, $start_t, $filename ) = @_;
-
-	my $perc = 0;
-	$perc = int(($bytesdown / $expectedbytes) * 100) if ( $expectedbytes != 0 );
-        my $speed = 0;
-	$speed = $bytesdown/((time - $start_t)) if ( ((time - $start_t) > 0) and  (defined $start_t and $start_t ne 0) );
-        my $secs_left = "n/a";
-	$secs_left = int((($expectedbytes - $bytesdown) / $speed) + .5) if ( $speed ne "0" );
-        $speed = int($speed/1024);
-	# status is a function reference, call it.
-	&{$status}( $perc, $speed, $secs_left, $bytesdown, $expectedbytes, $filename);
-}
-
-# The following functions provide the basis for offline (non interactive) downloading.
-# i.e. the files are downloaded without being attached to a front end (such as perl etc)
-
-sub checkstatus
-{
-	my ( $url, $filename, $size, $md5, $status ) = @_;
-
-	my $destination = "$downloadstore/$filename";
-
-	# check for a "failure"
-	if ( -e "$destination.failure" ){
-		size_callback( -1, -1, $status, -1, $filename );	
-		return -3;
-	}
-
-	unless ( -e $destination ){
-		return 1;
-	}
-
-	my $gotsize = ( stat( "$destination") )[7];
-
-	my $date = (stat( "$destination" ))[9];
-	
-	if ( ((time() - $date) > 60 ) and ( $gotsize != $size ) ){
-		# the download has probably stalled...
-		size_callback( -1, $size, $status, -1, $filename );	
-		return -2;
-	}
-
-	if ( $gotsize == $size ){
-		# file is complete, check it's md5
-		if ( not checkmd5( $filename, $md5 ) ){
-			size_callback( -1, $size, $status, -1, $filename );	
-			return -1;
-		}
-	}
-
-	my $start_t = 0;
-
-	# get the timestamp for this file...
-        if ( open(TSFILE, "<$destination.timestamp") ){
-		$start_t = <TSFILE>;
-		chomp $start_t;
-		close TSFILE;
-	};
-	
-
-	size_callback( $gotsize, $size, $status, $start_t, $filename );	
-	
-	return 0;
-}
+my $download_store = "/var/patches/downloads/";
+my $progress_store = "/var/patches/pending/";
 
 sub checkmd5
 {
@@ -261,5 +44,245 @@ sub checkmd5
 	}
 }
 
+sub download
+{
+	my ( $base, $file ) = @_;
+	print STDERR "Going for download... ($file)\n";
+
+	# invoke wget to download a file and store accordingly.
+
+	my $final = "$progress_store$file";
+	my $log   = "$progress_store$file.log";
+	my $pid   = "$progress_store$file.pid";
+
+	my @commands = ( "/usr/bin/wget", "-o", "$log", "-b", "--progress=bar", "-O", "$final", "$base$file" );
+
+	my ( $status, $pid_out );
+	open(PIPE, '-|') || exec( @commands );
+        while (<PIPE>) { 
+		$status .= $_; 
+	}
+	close(PIPE);
+
+	# determine PID information (which was told to us by wget
+	my ( $pid_number ) = ( $status =~ /.*pid\s+(\d+)\..*/m );
+	
+	print STDERR "wget returned $status\n";
+
+	unless ( open ( $pid_out, ">$pid" ) ){
+		print STDERR "Unable to allocate PID $pid\n";
+		kill $pid_number;
+		return "cannot allocate pid";
+	}
+
+	print $pid_out "$pid_number";
+	close $pid_out;
+
+	sleep( 3 ); # give things a chance to start
+
+	print STDERR "Got the reply from wget as $status\n";
+	print STDERR "Going to $final and $log $pid ($pid_number)\n";
+
+}
+
+sub progress
+{
+	my ( $file ) = @_;
+
+	# check on the status of the download of $file
+	my ( $status_file, $status );
+
+	my $log   = "$progress_store$file.log";
+
+	unless( open ( $status_file, "<$log" ) ){
+		if ( -e "$download_store$file" ){
+			# download is completed ...
+			return ( "", "100%", "-", "$download_store$file" );
+		}
+
+		print STDERR "Cannot open $log\n";
+		return "error";
+	}
+
+	my ( $down, $percent, $speed, $complete ) = ( 0, "0px", 0, "" );
+
+	while ( $status = <$status_file> ) { 
+		my ( $ddown, $dpercent, $dspeed ) = ( $status =~ /(\d+K|M)[^\d]*(\d+%)\s+(\d+\.\d+ ..\/s)/ );
+		if ( defined $ddown ){
+			$down = $ddown;
+			$percent = $dpercent;
+			$speed = $dspeed;
+		}
+		if ( $percent eq "100%" ){
+			print STDERR "Checking for completion $status\n";
+			if ( $status =~ /`([^`]*)'/ ){
+				$complete = $1;
+			}
+		}	
+	};
+
+	# status should now be the last line of the status ...
+
+	if ( $complete eq "$progress_store$file" ){
+		# download is complete, move the file to the download cache
+		# and then tidy up.
+		my $final = "$download_store$file";
+		move( "$progress_store$file", $final );
+		downloadtidy( $file );
+		return ( $down, $percent, $speed, $final );
+	}
+
+	return ( $down, $percent, $speed );
+}
+
+sub cancel 
+{
+	my ( $file ) = @_;
+	
+	# cancel a download, killing the process (if it's running)
+
+	#retrieve the process ID
+	my $pid   = "$progress_store$file.pid";
+	my $pid_in;
+
+	unless ( open ( $pid_in, "<$pid" ) ){
+		return "cannot get pid";
+	}
+	
+	my $pid_number = <$pid_in>;
+	close $pid_in;
+
+	# check that this PID is a) running and b) belongs to wget...
+
+	if (open(FILE, "/proc/${pid_number}/status"))
+	{
+		while (<FILE>)
+		{
+			if (/^Name:\W+(.*)/) {
+				$testcmd = $1; }
+		}
+		close FILE;
+		if ($testcmd =~ /wget/)
+		{
+			print STDERR "W000T found something to kill $pid_number\n";
+			kill 15, $pid_number;
+		} else {
+			print STDERR "This is not the process you're looking for\n";
+		}
+	} else {
+		print STDERR "No process to kill, guess it's not there\n";
+	}	
+
+	downloadtidy( $file );
+}
+
+sub downloadtidy
+{
+	# tidy up the PID and progress files for the relevant file.
+
+	my ( $file ) = @_;
+
+	my $final = "$progress_store$file";
+	my $log   = "$progress_store$file.log";
+	my $pid   = "$progress_store$file.pid";
+	
+	unlink $log;
+	unlink $pid;
+	unlink $final;
+
+}
+
+sub progress_bar
+{
+	# create a progress bar to display the download progress
+	# and management of the file $file ($_[0])
+
+	my ( $file ) = @_ ;
+
+	print <<END
+<table class='centered'>
+<tr>
+	<form method='post'>
+	<td>
+		<table class='progressbar'>
+			<tr>
+				<td id='progress-$file' style='background-color: blue; width: 1px;'>&nbsp;</td>
+				<td class='progressend'>&nbsp;</td>
+			</tr>
+		</table>
+	</td>
+	<td rowspan='2' style='vertical-align: top;'>
+END
+;
+
+	# get the intial status and render controls accordingly.
+
+	my ( $down, $percent, $speed, $complete ) = &progress( $file );
+
+	if ( $percent eq "100%" ){
+		# download is complete... hmm
+	} elsif( not defined $percent or $percent eq "" ){
+		# there was an error somewhere, or we don't have it
+		print <<END
+	<input type='hidden' name='file' value='$file'>
+	<input type='submit' name='download' value='download'>
+END
+;
+	} else {
+		# we are currently downloading the thing, we can issue a cancel if we want
+		print <<END
+	<input type='hidden' name='file' value='$file'>
+	<input type='submit' name='cancel' value='cancel'>
+END
+;
+	}
+
+print <<END
+	</td>
+</tr>
+<tr>
+	<td><span id='status-$file'></span></td>
+</tr>
+</form>
+END
+;
+	
+}
+
+sub update_bar
+{
+	my ( $file ) = @_;
+
+	my ( $down, $percent, $speed, $complete ) = &progress( $file );
+
+	print <<END
+<script>
+	document.getElementById('progress-$file').style.width = '${percent};';
+</script>
+END
+;
+
+	if ( $percent eq "100%" ){
+		return 0;
+	} elsif( not defined $percent or $percent eq "" ){
+		return -1;
+	} else {
+		return 1;
+	}
+}
+
+sub clear_download_cache
+{
+	opendir( my $dir, "/var/patches/downloads" );
+
+	my @list = readdir( $dir );
+
+	closedir( $dir );
+
+	foreach my $file ( @list ){
+		next if ( $file =~ /^\..*/ );
+		unlink( "/var/patches/downloads/$file" );
+	}
+}
 
 1;
