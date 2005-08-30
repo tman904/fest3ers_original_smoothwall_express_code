fee78a09fbf2398341b9e69be6595f72a39838dd
darren <darren@2e32aa7d-f303-0410-ac85-8a20d3371fc6>
2005-08-30 08:20:26 +0000

    Updated teh regbot to take more information
    git-svn-id: https://smoothwall.svn.sourceforge.net/svnroot/smoothwall/trunk@123 2e32aa7d-f303-0410-ac85-8a20d3371fc6

--- distrib/build/sources/coretree/tree/usr/bin/smoothwall/machine_reg.pl
+++ distrib/build/sources/coretree/tree/usr/bin/smoothwall/machine_reg.pl
@@ -5,10 +5,30 @@ use header qw( :standard );
 
 my (%eth,%isdn,%pppsettings);
 
+# detail connection details, this is how the smoothie connects to the
+# outside world.  We have no interest in passwords or anything of that
+# ilk of course, but knowing modem types and drivers is always good
+# for support purposes.
+
+# determine the Ethernet Settings
+
 &readhash("${swroot}/ethernet/settings", \%eth);
+
+# And the ISDN Settings (if enabled)
+
 &readhash("${swroot}/isdn/settings", \%isdn);
+
+# PPP Settings
+
 &readhash("${swroot}/ppp/settings", \%pppsettings);
+
+# ADSL Settings
+
 &readhash("${swroot}/adsl/settings", \%adslsettings);
+
+# Retrieve details about the CPU, again this is for general purpose
+# information about what to support (can we finally drop i386 support
+# and turn on some optimisations ? )
 
 open(CPU, "/proc/cpuinfo") or die "Could not open /proc/cpuinfo";
 my ($junk,$mhz,$model,$vid);
@@ -27,6 +47,8 @@ $vid =~ s/\s+$//;
 $model =~ s/^\s+//;
 $model =~ s/\s+$//;
 
+# Detail memory use and amounts on the machine.
+
 open(MEM, "/proc/meminfo") or die "Could not open /proc/meminfo";
 my ($lp);
 while(<MEM>)
@@ -38,6 +60,9 @@ $lp =~ s/^\s+//;
 $lp =~ s/\s+$//;
 my $mem;
 ($mem,$junk) = split(/\s/,$lp);
+
+# Partitioning information, should we start worrying about logging
+# and come up with new methods of storing it etc..
 
 open(DISK, "/proc/partitions") or die "Could not open /proc/partitions";
 my (@this,$disk);
@@ -54,15 +79,69 @@ while(<DISK>)
 }
 close(DISK);
 
-my $extra = "ADSL_DEVICE=$adslsettings{'DEVICE'},ADSL_ECITYPE=$adslsettings{'ECITYPE'},ISDN_TYPE=$isdnsettings{'TYPE'}";
-my $info = "cpu_vid=$vid&cpu_model=$model&cpu_mhz=$mhz&mem=$mem&hdd=$disk&inst_type=$eth{'CONFIG_TYPE'}&isdn=$pppsettings{'COMPORT'}&version=$version-$revision&extra=$extra";
-$info =~ s/\s//g;
+# discover some LSPCI information, this will give us an indication of the sort
+# of hardware we're coming up against and the sort of drivers we need to 
+# support.
+my $lspci;
+
+open(PIPE, '-|') || exec( '/sbin/lspci' );
+while ( my $line = <PIPE>) { 
+	chomp $line;
+	my ( $busid, $type, $name ) = ( $line =~ /([^\s]+)\s+([^:]+):\s+(.*)/ );
+	$lspci .= "$busid|$type|$name||";
+}
+close(PIPE);
+
+# discover various bits of information about the modules which are loaded
+# this will give some hint as to how well the driver discovery code is working
+# amongst other things.
+
+my $lsmod;
+
+open(PIPE, '-|') || exec( '/sbin/lsmod' );
+while ( my $line = <PIPE>) { 
+	chomp $line;
+	my ( $driver, $size, $usedby ) = ( $line =~ /([^\s]+)\s+([^\s]+)\s+(.*)/ );
+	$lsmod .= "$driver|$size|$usedby||";
+}
+close(PIPE);
+
+# discover various interesting things about the USB Bus, this is in a 
+# perpetual state of flux and a widish range of details can only serve
+# to make sense of some of it.
+
+open(USB, "/proc/bus/usb/devices") or die "Could not open /proc/bus/usb/devices";
+
+my $usbbus;
+
+while( my $line = <USB>)
+{
+	chomp $line;
+	$line =~s/#//g;
+	$usbbus .= "$line|";
+}
+close(USB);
+
+# construct the additional information.
+
+my $extra;
+
+$extra .= "ADSL_DEVICE=$adslsettings{'DEVICE'}&";
+$extra .= "ADSL_ECITYPE=$adslsettings{'ECITYPE'}&";
+$extra .= "ISDN_TYPE=$isdnsettings{'TYPE'}&";
+$extra .= "LSMOD=$lsmod&";
+$extra .= "LSPCI=$lspci&";
+$extra .= "USBBUS=$usbbus";
+
+my $info = "cpu_vid=$vid&cpu_model=$model&cpu_mhz=$mhz&mem=$mem&hdd=$disk&inst_type=$eth{'CONFIG_TYPE'}&isdn=$pppsettings{'COMPORT'}&version=$version-$revision$extra";
+$info =~ s/\s/%20/g;
+
 my $length = length($info);
 my $xhost = "www.smoothwall.org";
 
 use IO::Socket;
 $sock = new IO::Socket::INET ( PeerAddr => $xhost, PeerPort => 80, Proto => 'tcp', Timeout => 5 ) or die "Could not connect to host\n\n";
-print $sock "GET http://www.smoothwall.org/cgi-bin/machine_reg.cgi?$info HTTP/1.1\n";
+print $sock "GET http://www.smoothwall.org/cgi-bin/express3.cgi?$info HTTP/1.1\n";
 print $sock "Host: www.smoothwall.org\n\n";
 undef $/;
 $retsrt = <$sock>;
