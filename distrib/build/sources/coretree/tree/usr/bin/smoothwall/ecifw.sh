fbc57ab42c532880aa315c2cc466c860ad93823b
aslak <aslak@2e32aa7d-f303-0410-ac85-8a20d3371fc6>
2007-02-22 16:14:48 +0000

    Updates for eagle-usb and eci.  Both should now work (hopefully).
    git-svn-id: https://smoothwall.svn.sourceforge.net/svnroot/smoothwall/trunk@525 2e32aa7d-f303-0410-ac85-8a20d3371fc6

--- distrib/build/sources/coretree/tree/usr/bin/smoothwall/ecifw.sh
+++ distrib/build/sources/coretree/tree/usr/bin/smoothwall/ecifw.sh
@@ -4,5 +4,6 @@
 . /var/smoothwall/adsl/eci/$ECITYPE
 
 echo "Modem is a $FULLNAME"
-/usr/bin/eci-load1 $VID1 $PID1 $VID2 $PID2 /etc/eciadsl/$FIRM1
-/usr/bin/eci-load2 -t 120 $VID2 $PID2 /etc/eciadsl/$FIRM2
+/usr/bin/eciadsl-firmware $VID1 $PID1 $VID2 $PID2 /etc/eciadsl/$FIRM1
+/usr/bin/eciadsl-synch -mc GS7470 $VID2 $PID2 /etc/eciadsl/$FIRM2
+
