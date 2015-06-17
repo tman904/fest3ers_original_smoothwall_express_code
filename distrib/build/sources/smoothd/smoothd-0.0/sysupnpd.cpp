1cf33864953a8af563cd90c3cee7748e6aa41415
NPMurphy <neal.p.murphy@alum.wpi.edu>
2015-06-16 23:28:12 -0400

    Tweak for new miniupnpd. No CLI options, use just the unadorned program name. Always write the config file when starting. Added new 'upnpdwrite' function to write only the config file.

--- distrib/build/sources/smoothd/smoothd-0.0/sysupnpd.cpp
+++ distrib/build/sources/smoothd/smoothd-0.0/sysupnpd.cpp
@@ -26,6 +26,7 @@ extern "C" {
 	int restart_upnpd(std::vector<std::string> & parameters, std::string & response );
 	int start_upnpd(std::vector<std::string> & parameters, std::string & response );
 	int stop_upnpd(std::vector<std::string> & parameters, std::string & response );
+	int write_upnpd(std::vector<std::string> & parameters, std::string & response );
 }
 
 int load(std::vector<CommandFunctionPair> & pairs )
@@ -34,10 +35,12 @@ int load(std::vector<CommandFunctionPair> & pairs )
 	CommandFunctionPair restart_upnpd_function("upnpdrestart", "restart_upnpd", 0, 0);
 	CommandFunctionPair start_upnpd_function("upnpdstart", "start_upnpd", 0, 0);
 	CommandFunctionPair stop_upnpd_function("upnpdstop", "stop_upnpd", 0, 0);
+	CommandFunctionPair write_upnpd_function("upnpdwrite", "write_upnpd", 0, 0);
 
 	pairs.push_back(restart_upnpd_function);
 	pairs.push_back(start_upnpd_function);
 	pairs.push_back(stop_upnpd_function);
+	pairs.push_back(write_upnpd_function);
 
 	return 0;
 }
@@ -71,57 +74,62 @@ int stop_upnpd(std::vector<std::string> & parameters, std::string & response)
 int start_upnpd(std::vector<std::string> & parameters, std::string & response)
 {
 	int error = 0;
+
+	ConfigVAR settings("/var/smoothwall/advnet/settings");
+
+	// Do nothing if not enabled
+	if (settings["ENABLE_UPNP"] != "on")
+	{
+		return error;
+	}
+
+	// If RED is not active, UPnP won't work anyway
 	ConfigSTR iface("/var/smoothwall/red/iface");
+	if (iface.str() == "")
+	{
+		return error;
+	}
+
+	// We're here; run it
+
 	ConfigSTR uuid("/etc/miniupnpd.uuid");
 	std::vector<std::string> args;
-	ConfigVAR settings("/var/smoothwall/advnet/settings");
 	ConfigVAR netsettings("/var/smoothwall/ethernet/settings");
 	ConfigVAR ownershipsettings("/var/smoothwall/main/ownership");
 	ConfigVAR productdatasettings("/var/smoothwall/main/productdata");
 
+	// Write the config
+	args.push_back("/usr/bin/smoothwall/writeupnp");
+	error = simplesecuresystemvector(args);
+	if (error)
+	{
+		response = "Can't write miniupnpd.conf; won't start daemon";
+		return error;
+	}
+	args.clear();
+
+	// Start the daemon
 	args.push_back("/usr/sbin/miniupnpd");
 	args.push_back("-f");
 	args.push_back("/etc/miniupnpd.conf");
-	args.push_back("-i");
-	args.push_back(iface.str());
-	args.push_back("-u");
-	args.push_back(uuid.str());
-	args.push_back("-U");
-	args.push_back("-w");
-	args.push_back(stringprintf("https://%s:441", netsettings["GREEN_ADDRESS"].c_str()));
-	args.push_back("-m");
-	args.push_back(stringprintf("%s-%s-%s", productdatasettings["VERSION"].c_str(),
-		productdatasettings["REVISION"].c_str(), productdatasettings["ARCH"].c_str()));
-	args.push_back("-s");
-	if (ownershipsettings["ID"] != "")
-		args.push_back(ownershipsettings["ID"]);
+	if (error)
+		response = "Can't start miniupnpd";
 	else
-		args.push_back("0");
-	
-	if (netsettings["GREEN_DEV"] != "")
+		response = "Miniupnpd start successful";
+	return error;
+}
+
+int write_upnpd(std::vector<std::string> & parameters, std::string & response)
+{
+	int error = 0;
+	std::vector<std::string> args;
+
+	// Write the config
+	args.push_back("/usr/bin/smoothwall/writeupnp");
+	error = simplesecuresystemvector(args);
+	if (error)
 	{
-		args.push_back("-a");
-		args.push_back(netsettings["GREEN_ADDRESS"]);
+		response = "Can't write miniupnpd.conf";
 	}
-	if (netsettings["PURPLE_DEV"] != "")
-	{
-		args.push_back("-a");
-		args.push_back(netsettings["PURPLE_ADDRESS"]);
-	}
-		
-	/* Not an error really. */
-	if (iface.str() == "")
-		goto EXIT;
-	
-	if (settings["ENABLE_UPNP"] == "on")
-	{
-		error = simplesecuresystemvector(args);
-		if (error)
-			response = "Can't start minipnpd";
-		else
-			response = "Miniupnpd start successful";
-	}
-	
-EXIT:
 	return error;
 }
