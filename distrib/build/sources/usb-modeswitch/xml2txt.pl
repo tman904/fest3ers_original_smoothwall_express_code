f441387f26192ada454b905b1ac3f8f1efcc6a7a
NPMurphy <neal.p.murphy@alum.wpi.edu>
2016-01-08 10:59:27 -0500

    Oops. Need xml2txt.pl to fetch sources.

---
+++ distrib/build/sources/usb-modeswitch/xml2txt.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# Script to illustrate how to parse a simple XML file
+# and dump its contents in a Perl hash record.
+
+#use strict;
+use XML::Simple;
+use Data::Dumper;
+
+my $what = $ARGV[0];
+my $which = $ARGV[1];
+
+if ( ! -f "usb-modeswitch-versions.xml" )
+{
+  system("wget http://www.draisberghof.de/usb_modeswitch/usb-modeswitch-versions.xml");
+}
+
+my $modeswitch = XMLin('usb-modeswitch-versions.xml', KeyAttr => {target => 'URL'});
+
+#print STDERR Dumper($modeswitch);
+
+my $i;
+for ($i=0; $i<@{$modeswitch->{"package"}}; $i++)
+{
+  if ($modeswitch->{"package"}->[$i]->{'name'} eq $which)
+  {
+    print $modeswitch->{"package"}->[$i]->{$what};
+    exit 0;
+  }
+}
