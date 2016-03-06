ab02c1567f0b04056d14de7b78b769a07873e9cf
NPMurphy <neal.p.murphy@alum.wpi.edu>
2016-03-05 23:08:26 -0500

    Patch httpd.conf to fix existing httpd.conf: bug 164.

--- distrib/build/sources/updates/update7/patch-apache-conf.sh
+++ distrib/build/sources/updates/update7/patch-apache-conf.sh
@@ -1,17 +1,15 @@
 #! /bin/sh
 
 # If dontlog is found, the patch has already been applied
-if grep dontlog /etc/httpd/conf/httpd.conf >/dev/null 2>&1; then
+if grep "conf/magic" /etc/httpd/conf/httpd.conf >/dev/null 2>&1; then
   echo "httpd.conf already patched"
   exit
 
 else
 
   # Patch apache's conf
-  sed -i -e '/^ErrorLog/a\
-SetEnvIf Request_URI "time-clock.cgi$" dontlog\
-SetEnvIf Request_URI "trafficstats.cgi" dontlog
-s/\(CustomLog.*common$\)/\1 env=!dontlog/' /etc/httpd/conf/httpd.conf
+  sed -i -e 's=/usr/share/magic=/etc/httpd/conf/magic=' \
+      /etc/httpd/conf/httpd.conf
   echo "httpd.conf patched"
 
   # Restart apache
