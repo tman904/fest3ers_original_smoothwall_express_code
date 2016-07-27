3d86636c58905d543ba6c20366531f44edf880fb
NPMurphy <neal.p.murphy@alum.wpi.edu>
2016-07-26 23:34:44 -0400

    --pid-path has *two* leading dashes.

--- distrib/build/sources/smoothd/smoothd-0.0/syssnort.cpp
+++ distrib/build/sources/smoothd/smoothd-0.0/syssnort.cpp
@@ -145,7 +145,7 @@ int start_snort(std::vector<std::string> & parameters, std::string & response)
 	if (settings["ENABLE_SNORT"] == "on")
 	{
 		error = simplesecuresysteml("/usr/bin/snort","-c", "/etc/snort.conf", "-D",
-			"-u", "snort", "-m", "033", "-pid-path=/var/run", "-g", "snort", "-d",  "-e", "-A", "Full",
+			"-u", "snort", "-m", "033", "--pid-path=/var/run", "-g", "snort", "-d",  "-e", "-A", "Full",
 			"-i" , iface.str().c_str(), NULL);
 
 		if (error)
