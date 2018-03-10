413daa24397c64588277cf8cb688c60acecc2030
NPMurphy <neal.p.murphy@alum.wpi.edu>
2018-03-09 20:55:27 -0500

    Change wget to use https. Seems more and more hosts refuse to allow HTTP any more.

--- distrib/build/toolcrib/get-snort-ver.sh
+++ distrib/build/toolcrib/get-snort-ver.sh
@@ -11,7 +11,7 @@ if [ $# -ne 2 ]; then
 fi
 
 # Deal with snort's eccentricity. Look up the latest version and return it.
-ANCHOR=`wget -q -o /dev/null -O- "http://www.snort.org/downloads/" \
+ANCHOR=`wget -q -o /dev/null -O- "https://www.snort.org/downloads/" \
        | egrep "${2}-[0-9.]*tar\.gz" \
        | head -1`
 
